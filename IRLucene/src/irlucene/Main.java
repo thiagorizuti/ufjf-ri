6e795856faa29a4d71b067f89b6b961912c59799
Thiago Costa Rizuti da Rocha <t.rizuti@gmail.com>
2016-05-28 18:26:07 -0300

    indexing and querying

--- IRLucene/src/irlucene/Main.java
+++ IRLucene/src/irlucene/Main.java
@@ -15,11 +15,14 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -27,6 +30,7 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
@@ -37,63 +41,69 @@ import org.apache.lucene.util.Version;
  * @author thiago
  */
 public class Main {
-    
-    
-    public static void main(String args[]) throws MalformedURLException{
-        Reader reader = new Reader();
-        ArrayList<CFCDocument> docs1 = reader.readCFCFile("cf74");
-        ArrayList<CFCDocument> docs2 = reader.readCFCFile("cf75");
-        ArrayList<CFCDocument> docs3 = reader.readCFCFile("cf76");
-        ArrayList<CFCDocument> docs4 = reader.readCFCFile("cf77");
-        ArrayList<CFCDocument> docs5 = reader.readCFCFile("cf78");
-       ArrayList<CFCDocument> docs6 = reader.readCFCFile("cf79");
-        ArrayList<MEDDocument> docs0 = reader.readMEDFile("MED.ALL");
-        int i = 1;
-        /*for(CFCDocument doc : docs3){
-            System.out.println(i + doc.getAuthors());
-            i++;
-        }*/
-        for(MEDDocument doc : docs0){
-            System.out.println(i + " " + doc.getId());
-            i++;
-        }
-        /*try {
-            
-            FileReader fr = new FileReader("data/cfc/");
-            fr.readFile("cf74");
+
+    public static void main(String args[]) throws MalformedURLException {
+        try {
+            Reader reader = new Reader();
+            ArrayList<CFCDocument> CFCdocs = new ArrayList();
+            CFCdocs.addAll(reader.readCFCFile("cf74"));
+            CFCdocs.addAll(reader.readCFCFile("cf75"));
+            CFCdocs.addAll(reader.readCFCFile("cf76"));
+            CFCdocs.addAll(reader.readCFCFile("cf77"));
+            CFCdocs.addAll(reader.readCFCFile("cf78"));
+            CFCdocs.addAll(reader.readCFCFile("cf79"));
+            ArrayList<MEDDocument> docs0 = reader.readMEDFile("MED.ALL");
+            int i = 1;
+            /*for (CFCDocument doc : CFCdocs) {
+                System.out.println(i + " " + doc.getPaperNumber());
+                i++;
+            }
+            for (MEDDocument doc : docs0) {
+                System.out.println(i + " " + doc.getId());
+                i++;
+            }*/
+
+            Directory index = FSDirectory.open(Paths.get("index/"));
             Analyzer analyzer = new StandardAnalyzer();
-            
-            // Store the index in memory:
-            //Directory directory = new RAMDirectory();
-            // To store an index on disk, use this instead:
-            Directory directory = FSDirectory.open(Paths.get("index/"));
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
-            IndexWriter iwriter = new IndexWriter(directory, config);
-            Document doc = new Document();
-            String text = "This is the text to be indexed.";
-            doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
-            iwriter.addDocument(doc);
-            iwriter.close();
-            
-            /// Now search the index:
-            DirectoryReader ireader = DirectoryReader.open(directory);
-            IndexSearcher isearcher = new IndexSearcher(ireader);
-            // Parse a simple query that searches for "text":
-            QueryParser parser = new QueryParser("fieldname", analyzer);
-            Query query = parser.parse("text");
-            //ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-            //assertEquals(1, hits.length);
-            // Iterate through the results:
-            //for (int i = 0; i < hits.length; i++) {
-                //Document hitDoc = isearcher.doc(hits[i].doc);
-                //assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-           // }
-            ireader.close();
-            directory.close();
-        } catch (IOException ex) {
+            IndexWriter indexWriter = new IndexWriter(index, config);
+            Document document;
+            for (CFCDocument doc : CFCdocs) {
+                document = new Document();
+                document.add(new StringField("paperNumber", doc.getPaperNumber(), Field.Store.YES));
+                document.add(new StringField("recordNumber", doc.getRecordNumber(), Field.Store.YES));
+                document.add(new StringField("acessionNumber", doc.getAcessionNumber(), Field.Store.YES));
+                document.add(new TextField("auhtors", doc.getAuthors(), Field.Store.YES));
+                document.add(new TextField("title", doc.getTitle(), Field.Store.YES));
+                document.add(new TextField("source", doc.getSource(), Field.Store.YES));
+                document.add(new TextField("majorSubjects", doc.getMajorSubjects(), Field.Store.YES));
+                document.add(new TextField("minorSubjects", doc.getMinorSubjects(), Field.Store.YES));
+                document.add(new TextField("abstractExtract", doc.getAbstractExtract(), Field.Store.YES));
+                document.add(new TextField("references", doc.getReferences(), Field.Store.YES));
+                document.add(new TextField("citations", doc.getCitations(), Field.Store.YES));
+                indexWriter.addDocument(document);
+            }
+            indexWriter.close();
+            String querystr = "What are the effects of calcium on the physical "
+                    + "properties of mucus from CF patients?";
+            Query q = new QueryParser("title", analyzer).parse(querystr);
+            int hitsPerPage = 10;
+            IndexReader indexReader = DirectoryReader.open(index);
+            IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+            TopDocs docs = indexSearcher.search(q, hitsPerPage);
+            ScoreDoc[] hits = docs.scoreDocs;
+            System.out.println("Found " + hits.length + " hits.");
+            for (i = 0; i < hits.length; ++i) {
+                int docId = hits[i].doc;
+                Document d = indexSearcher.doc(docId);
+                System.out.println((i + 1) + ". " + d.get("paperNumber") + "\t" + d.get("title"));
+            }
+            indexReader.close();
+            index.close();
+        } catch (IOException | ParseException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParseException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+
     }
+
 }
